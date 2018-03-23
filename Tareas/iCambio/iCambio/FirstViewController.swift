5d58b88d438cdc2f59b293859adf0d8e9560cda7
Brandon Vargas <j.brandon.v.m@gmail.com>
2018-03-23 16:17:23 -0600

    Added app project iCambio

---
+++ Tareas/iCambio/iCambio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iCambio
+//
+//  Created by José Brandon Vargas Mariñelarena on 10/03/18.
+//  Copyright © 2018 José Brandon Vargas Mariñelarena. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tareas/iCambio/iCambio/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iCambio
+//
+//  Created by José Brandon Vargas Mariñelarena on 10/03/18.
+//  Copyright © 2018 José Brandon Vargas Mariñelarena. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

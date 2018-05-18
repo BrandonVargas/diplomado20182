b704ca014d886de279731c0da7b962ab0a98eae6
Brandon Vargas <j.brandon.v.m@gmail.com>
2018-05-18 18:40:05 -0500

    Chapter 15 Camera

---
+++ Tareas/HomePwner/HomePwner/ImageStore.swift
@@ -0,0 +1,9 @@
+//
+//  ImageStore.swift
+//  HomePwner
+//
+//  Created by José Brandon Vargas Mariñelarena on 18/05/18.
+//  Copyright © 2018 José Brandon Vargas Mariñelarena. All rights reserved.
+//
+
+import Foundation
---
+++ Tareas/HomePwner/HomePwner/ItemsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomePwner
+//
+//  Created by José Brandon Vargas Mariñelarena on 18/05/18.
+//  Copyright © 2018 José Brandon Vargas Mariñelarena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
